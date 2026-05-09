149abcaca82ba3a5e1c60e5ec59e2a8ee9cab447
Joshua Zacek <personal.joshuazacek@gmail.com>
2026-05-09 01:33:21 +0100

    Created simple CNN in model.py

--- model.py
+++ model.py
@@ -1,8 +1,20 @@
-import torch
+from torch import nn
 
-class NeuralNet(torch.nn.Module):
+class NeuralNet(nn.Module):
     def __init__(self):
         super().__init__()
 
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, padding=1)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.relu = nn.ReLU()
+        self.fc1 = nn.Linear(32 * 112 * 112, 37)
+
     def forward(self, inputs):
-        return inputs
+        inputs = self.conv1(inputs)
+        inputs = self.pool(inputs)
+        inputs = self.relu(inputs)
+
+        inputs = inputs.view(-1, 32 * 112 * 112)
+        outputs = self.fc1(inputs)
+
+        return outputs
