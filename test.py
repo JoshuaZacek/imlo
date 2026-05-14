46685702cc61e53651ef635da883d695c7f73c64
Joshua Zacek <personal.joshuazacek@gmail.com>
2026-05-14 14:23:47 +0100

    Added cuda support for test.py

--- test.py
+++ test.py
@@ -4,10 +4,9 @@ from model import PetBreedClassifier
 from dataloader import initDataLoader
 from eval import evaluateModel
 
-# select compute device
-device = torch.device(
-    "mps" if torch.backends.mps.is_available() else "cpu"
-)
+# select compute device (Apple silicon, Nvidia GPU, or CPU)
+deviceType = "mps" if torch.backends.mps.is_available() else "cuda" if torch.cuda.is_available() else "cpu"
+device = torch.device(deviceType)
 print(f"using device: {device}")
 
 # load dataset
